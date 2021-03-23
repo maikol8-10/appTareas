5437f51852849a3993c53ab0c368a6c0b2f0038b
Michael Vallejos <maikol8-10hotmail.com>
2021-03-23 16:58:09 -0600

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from "./componentes/header";
+
+const App = () => {
+    return (
+        <div className="contenedor">
+            <Header/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/componentes/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEyeSlash} from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <h1 className="header__titulo">Lista de tareas</h1>
+            <button className="header__boton"> No mostrar completadas
+                <FontAwesomeIcon icon={faEyeSlash} className="header__icono-boton"></FontAwesomeIcon>
+            </button>
+        </header>
+    );
+}
+
+export default Header;
